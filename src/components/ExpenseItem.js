0badbdb38680f4c413eb1259c3b2c77b85342072
IuraCPersonal <iurie.cius.personal@gmail.com>
2023-04-05 09:56:09 +0300

    feat(src): Cleaned project Tree, added first compoent

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./ExpenseItem.css";
+
+const ExpenseItem = () => {
+  const expenseDate = new Date(2021, 2, 28);
+
+  return (
+    <div className="expense-item">
+      <div>{expenseDate}</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$294.67</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
