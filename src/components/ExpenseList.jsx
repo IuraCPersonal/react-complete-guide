fe7b4d81b87d0f576f83e33e82ea972853ef01b1
IuraCPersonal <iurie.cius.personal@gmail.com>
2023-04-11 10:18:06 +0300

    feat(*): fixed List Style, added Composition Card

---
+++ src/components/ExpenseList.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Card from "./Card";
+import ExpenseItem from "./ExpenseItem";
+
+import "./ExpenseList.css";
+
+const ExpenseList = (props) => {
+  return (
+    <Card className="expenses">
+      <div>
+        {props.expenses.map((element) => (
+          <ExpenseItem
+            date={element.date}
+            title={element.title}
+            amount={element.amount}
+          />
+        ))}
+      </div>
+    </Card>
+  );
+};
+
+export default ExpenseList;
