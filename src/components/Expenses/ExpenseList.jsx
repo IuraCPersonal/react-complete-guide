eac9bfda4aa1456f0d9ae584de294a33daf216ee
IuraCPersonal <iurie.cius.personal@gmail.com>
2023-04-13 09:35:45 +0300

    feat(ExpenseFilter): reading and passing filtered year

--- src/components/Expenses/ExpenseList.jsx
+++ src/components/Expenses/ExpenseList.jsx
@@ -1,22 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
+import ExpenseFilter from "./ExpenseFilter";
 
 import "./ExpenseList.css";
 
 const ExpenseList = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filteredYearChangeHandler = (newFilteredYear) => {
+    setFilteredYear(newFilteredYear);
+  };
+
   return (
-    <Card className="expenses">
-      <div>
-        {props.expenses.map((element) => (
-          <ExpenseItem
-            date={element.date}
-            title={element.title}
-            amount={element.amount}
-          />
-        ))}
-      </div>
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpenseFilter selected={filteredYear} onFilteredYearChange={filteredYearChangeHandler} />
+        <p>{filteredYear}</p>
+        <div>
+          {props.expenses.map((element) => (
+            <ExpenseItem
+              key={Math.random()}
+              date={element.date}
+              title={element.title}
+              amount={element.amount}
+            />
+          ))}
+        </div>
+      </Card>
+    </div>
   );
 };
 
