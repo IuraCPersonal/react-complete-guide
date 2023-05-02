ae2127a220d58188fd244edf9ff327bb335e8170
IuraCPersonal <iurie.cius.personal@gmail.com>
2023-05-02 23:21:23 +0300

    feat(store): using React Toolkit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,4 +1,5 @@
 import { useSelector, useDispatch } from 'react-redux';
+import { counterActions } from '../store';
 
 import classes from './Counter.module.css';
 
@@ -9,28 +10,19 @@ const Counter = () => {
   const show = useSelector(state => state.showCounter);
 
   const incrementHandler = () => {
-    dispatch({
-      type: 'increment'
-    })
+    dispatch(counterActions.increment());
   };
 
   const increaseHandler = () => {
-    dispatch({
-      type: 'increase',
-      amount: 10
-    });
+    dispatch(counterActions.increase({ amount: 10 }));
   };
 
   const decrementHandler = () => {
-    dispatch({
-      type: 'decrement'
-    })
+    dispatch(counterActions.decrement());
   };
 
   const toggleCounterHandler = () => {
-    dispatch({
-      type: 'toggle'
-    });
+    dispatch(counterActions.toggleCounter());
   };
 
   return (
